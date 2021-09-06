80faa2b49d82cfb1598d5adf1fffb58423ef0845
Larisa Moroz <lmoroz@users.noreply.github.com>
2021-09-06 12:58:51 +0800

    Switch to schedule user script

--- foxford.user.js
+++ foxford.user.js
@@ -1,152 +1,169 @@
 // ==UserScript==
-// @name         Foxford fill Teachers
-// @description  Расширение для сайта foxford.ru: выводит ИМЕНА ПРЕПОДАВАТЕЛЕЙ и даты начала-окончания курсов в списке программ обучения на https://foxford.ru/dashboard
+// @name         Foxford schedule time adjustment
 // @namespace    https://github.com/lmoroz/foxford
-// @downloadURL  https://raw.githubusercontent.com/lmoroz/foxford/master/foxford.user.js)
 // @version      0.1
 // @author       Larisa Morozhnikova
-// @match        https://foxford.ru/dashboard
+// @match        https://foxford.ru/dashboard*
+// @icon         https://www.google.com/s2/favicons?domain=foxford.ru
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+console.log('intercept');
 
-    let courses = [];
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const userTimeZoneOffset = (new Date().getTimezoneOffset())/60;
+const userLocale = Intl.DateTimeFormat().resolvedOptions().locale;
+const arrays = ['course_lessons', 'coach_lessons','events'];
 
-    const reformat = async function () {
+if (!XMLHttpRequest.prototype.getResponseText) {
+    XMLHttpRequest.prototype.getResponseText = Object.getOwnPropertyDescriptor(XMLHttpRequest.prototype, 'responseText').get;
+}
+Object.defineProperty(XMLHttpRequest.prototype, 'responseText', {
+    get: function() {
+        let responseText = XMLHttpRequest.prototype.getResponseText.call(this);
+        const req = this;
+        if(req.responseURL.match(/api\/calendar/)) {
+            const data = JSON.parse(responseText);
+            const arraysTotransform = [];
+            arrays.forEach(eventType => { if( eventType in data && data[eventType].length > 0) arraysTotransform.push(eventType); });
+            if(arraysTotransform.length > 0) {
+                arraysTotransform.forEach (eventType => {
+                    data[eventType].forEach(e => {
+                        if ('starts_at' in e) {
+                            //console.log(e.discipline_name || e.title, e.starts_at);
+                            const eTimeZoneOffset = e.starts_at.match(/([-+][0-9:]+)$/i)[1];
+                            let newStarsAt = moment(e.starts_at).format();
+                            const newTimeZoneOffset = newStarsAt.match(/([-+][0-9:]+)$/i)[1];
+                            e.starts_at = newStarsAt.replace(newTimeZoneOffset, eTimeZoneOffset);
+                            //console.log(e.discipline_name || e.title, eTimeZoneOffset, newTimeZoneOffset, e.starts_at);
+                        }
+                    });
+                });
+                responseText = JSON.stringify(data);
+                //console.log(responseText);
+            }
+        };
+        return responseText;
+    },
+    enumerable: true,
+    configurable: true
+});
 
-        fillCourses();
+function isIntersected (elt) {
+    const rect=elt.getBoundingClientRect();
+    const x=rect.left;
+    const y=rect.top;
+    const topElt=document.elementFromPoint(x,y);
+    //console.log({elt, topElt, x, y});
+    return (topElt && !topElt.isSameNode(elt)) ? topElt.outerHTML.trim() : elt.innerHTML.trim();
+}
 
-        const links = document.evaluate("//a[contains(., 'Добавить курс')]", document, null, XPathResult.ANY_TYPE, null );
-        const addLink = links.iterateNext();
+function reflow(calendarCellElements, calendarTimeElements) {
+    let timeCellCounter = -1;
+    let rowFilled = '';
+    for (let i = 0; i < calendarCellElements.length; i+=7) {
+        timeCellCounter++;
+        rowFilled = '';
+        const cellsToRemove = [];
+        for (let c = 0; c < 7; c++) {
+            const cellHTML = isIntersected (calendarCellElements[i+c]);
+            if (cellHTML > '') rowFilled += cellHTML;
+            else cellsToRemove.push(calendarCellElements[i+c]);
+        }
+        const timeCell = calendarTimeElements[timeCellCounter];
+        if (!rowFilled) {
+            //console.log(`remove cells fo row №${timeCellCounter+1} : `, cellsToRemove);
+            //console.log(`remove time row: ${timeCell.innerText}`);
+            cellsToRemove.forEach(cell => {cell.parentNode.removeChild(cell);});
+            timeCell.parentNode.removeChild(timeCell);
+        }
+        else {
+            //console.log(`Row № ${timeCell.innerText} is filled with`, rowFilled);
+            break;
+        }
+    }
+}
+function setHoursIds () {
+    document.querySelectorAll(`div[data-qa="table_Styled.Body"] div[data-qa="table_Styled.Item"]`).forEach ((el,id) => {
+        el.dataset.weekHourId = id;
+    });
+}
+function repostitionTimeLine() {
+    const calendarTimeLine = document.querySelector('div[data-qa="table_Styled.Line"]');
+    const dt = new Date();
+    if (calendarTimeLine && calendarTimeLine.parentNode) {
+        const currentCellId = (dt.getHours()*7-1)+dt.getDay();
+        const currentCell = document.querySelector(`div[data-qa="table_Styled.Body"] div[data-week-hour-id="${currentCellId}"]`);
+        if (currentCell && !currentCell.isSameNode(calendarTimeLine.parentNode)) {
+            console.log('repostitionTimeLine… move to', currentCell);
+            calendarTimeLine.parentNode.removeChild(calendarTimeLine);
+            currentCell.style.backgroundColor = 'lyghtyellow';
+            console.log(currentCell);
+            currentCell.appendChild(calendarTimeLine);
+            document.querySelector('div[data-qa="table_Scrollable"]>div').scrollTop = 0;
+        }
+    }
+    if (calendarTimeLine) {
+        const currentHourPercent = Math.round(dt.getMinutes()*100/60, 2);
+        console.log('repostitionTimeLine… to ', currentHourPercent, '%');
+        calendarTimeLine.style.top = `${currentHourPercent}%`;
+    }
+    window.setTimeout(repostitionTimeLine, 30000);
+}
 
-        if(!addLink || !courses.length) {
-            window.setTimeout(reformat, 1000);
+
+function reflowDelay() {
+    const observer = new MutationObserver(function (mutations, me) {
+        const calendarCellElements = document.querySelectorAll('div[data-qa="table_Styled.Body"] div[data-qa="table_Styled.Item"]');
+        const calendarTimeElements = document.querySelectorAll('div[data-qa="table_Styled.TimeWrapper"]>div');
+        const calendarCalendarCard = document.querySelectorAll('div[data-qa="calendar-card_Root"]');
+        const calendarTableBody = document.querySelector('div[data-qa="calendar-layout_Styled.Root"]');
+        const calendarTimeLine = document.querySelector('div[data-qa="table_Styled.Line"]');
+        if (calendarTimeLine) { setHoursIds(); repostitionTimeLine(); }
+        if (calendarTableBody && calendarCellElements.length && calendarTimeElements.length === 24 && calendarCalendarCard.length) {
+            console.log(`calendarTimeElements = `, calendarTimeElements.length);
+            console.log(`calendarCellElements = `, calendarCellElements.length);
+            console.log(`calendarCalendarCard = `, calendarCalendarCard.length);
+            reflow(Array.from(calendarCellElements), Array.from(calendarTimeElements));
+            me.disconnect(); // stop observing
+
+
+
+            const calendarTableBodyOobserver = new MutationObserver(function () {
+                const calendarCellElements = document.querySelectorAll('div[data-qa="table_Styled.Body"] div[data-qa="table_Styled.Item"]');
+                const calendarTimeElements = document.querySelectorAll('div[data-qa="table_Styled.TimeWrapper"]>div');
+                const calendarCalendarCard = document.querySelectorAll('div[data-qa="calendar-card_Root"]');
+                const calendarTimeLine = document.querySelector('div[data-qa="table_Styled.Line"]');
+                if (calendarCellElements.length && calendarTimeElements.length === 24 && calendarCalendarCard.length) {
+                    if (calendarTimeLine) { setHoursIds(); repostitionTimeLine(); }
+                    reflow(Array.from(calendarCellElements), Array.from(calendarTimeElements));
+                }
+
+            });
+
+            calendarTableBodyOobserver.observe(calendarTableBody, {
+                childList: true,
+                subtree: true
+            });
+
             return;
         }
+    });
 
-        const style = document.createElement('style');
-        style.textContent = `
-.teacherData {margin: 10px 0; color: #777; }
-.tooltipElement {display: flex; align-items; center;}
-.tooltipText {margin: 0 10px;}
-div.base-course { background: #48a1e624;}
-div.deep-course { background-color: #82d18024;}`;
-        document.head.append(style);
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    });
 
-        colorCourses();
-        await fillNextTeacher();
-    }
-
-    const fillCourses = function () {
-
-        courses = [];
-
-        const coursesBlocks = document.querySelectorAll('div[class^="CourseItem_root"]');
-        coursesBlocks.forEach(block => {
-            if (block.dataset.teacher) return;
-            let linkBlock = false; let link = false; let detailsBlock = false; let tooltipBlock = false;
-            linkBlock = block.querySelector('a');
-            detailsBlock = block.querySelector('div[class^="CourseItem_details"]');
-            tooltipBlock = block.querySelector('div[class^="CourseItem_tooltipWrapper"]');
-            console.log({block, tooltipBlock, detailsBlock});
-            if(linkBlock && detailsBlock && tooltipBlock)
-            {
-                link = 'https://foxford.ru/api'+linkBlock.getAttribute('href')+'/landing';
-                courses.push({block, link, detailsBlock, tooltipBlock});
-            }
-        });
-    }
-
-    function colorCourses () {
-        const baseCourseCards = [];
-        const deepCourseCards = [];
-        const baseSubTitles = ['Базовый уровень',
-                               'Эстрадная музыка',
-                               'Классическая музыка',
-                               'Второй год обучения',
-                              ];
-        const deepSubTitles = ['Углублённый уровень',
-                               'Текст в радость',
-                               'Мнемотехника',
-                               'астрономия',
-                               'шифры',
-                               'алгоритм',
-                               'программир',
-                              ];
-        try {
-            baseSubTitles.forEach(baseSubTitle => {
-                const baseSubTitleItems = document.evaluate("//h4[contains(translate(.,'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ','абвгдеёжзийклмнопрстуфхцчшщьыъэюя'), '" + baseSubTitle.toLowerCase() + "')]", document, null, XPathResult.ANY_TYPE, null );
-                let baseSubTitleElement = baseSubTitleItems.iterateNext();
-                console.log({baseSubTitle, baseSubTitleItems, baseSubTitleElement});
-                while (baseSubTitleElement) {
-                    const baseSubTitleCourseCard = baseSubTitleElement.closest('div[class^="CourseItem_root"]');
-                    console.log({baseSubTitle, baseSubTitleElement, baseSubTitleCourseCard});
-                    if (baseSubTitleCourseCard) baseCourseCards.push(baseSubTitleCourseCard);
-                    baseSubTitleElement = baseSubTitleItems.iterateNext();
-                }
-            });
-            deepSubTitles.forEach(deepSubTitle => {
-                const deepSubTitleItems = document.evaluate("//h4[contains(translate(.,'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ','абвгдеёжзийклмнопрстуфхцчшщьыъэюя'), '" + deepSubTitle.toLowerCase() + "')]", document, null, XPathResult.ANY_TYPE, null );
-                let deepSubTitleElement = deepSubTitleItems.iterateNext();
-                console.log({deepSubTitle, deepSubTitleItems, deepSubTitleElement});
-                while (deepSubTitleElement) {
-                    const deepSubTitleCourseCard = deepSubTitleElement.closest('div[class^="CourseItem_root"]');
-                    console.log({deepSubTitle, deepSubTitleElement, deepSubTitleCourseCard});
-                    if (deepSubTitleCourseCard) deepCourseCards.push(deepSubTitleCourseCard);
-                    deepSubTitleElement = deepSubTitleItems.iterateNext();
-                }
-            });
-            console.log({baseCourseCards, deepCourseCards});
-            baseCourseCards.forEach(card => card.classList.add('base-course'));
-            deepCourseCards.forEach(card => card.classList.add('deep-course'));
-        }
-        catch (e) {
-            console.error(e);
-        }
-    }
-
-    const fillTeachersOnScroll = async function (event) {
-        fillCourses();
-        colorCourses();
-        if(courses.length > 0 ) await fillNextTeacher();
-    }
-
-    const fillNextTeacher = async function () {
-        const currentBlock = courses.shift();
-        if(currentBlock && !currentBlock.block.dataset.teacher) {
-            const req = await fetch(currentBlock.link);
-            const resp = await req.json();
-            const teacher = resp.teachers[0];
-            const teacherName = `${teacher.last_name} ${teacher.first_name} ${teacher.middle_name}`;
-            const startAt = resp.starts_at;
-            const endAt = resp.finishes_at;
-            console.log(teacherName, startAt);
-
-            currentBlock.block.dataset.teacher = teacherName;
-
-            let teacherDataBlock = currentBlock.detailsBlock.querySelector('div.teacherData');
-            if(!teacherDataBlock) {
-                teacherDataBlock = document.createElement('div');
-                teacherDataBlock.classList.add('teacherData');
-                teacherDataBlock.innerHTML = `${teacherName}<br><small>с ${startAt} по ${endAt}</small>`;
-
-                currentBlock.detailsBlock.append(teacherDataBlock);
+}
 
 
-                const tooltipTextBlock = document.createElement('span');
-                tooltipTextBlock.classList.add('tooltipText');
-                tooltipTextBlock.innerHTML = `В архив`;
 
-                currentBlock.tooltipBlock.prepend(tooltipTextBlock);
-                currentBlock.tooltipBlock.classList.add('tooltipElement');
-            }
-        }
-        if(courses.length > 0 ) await fillNextTeacher();
-        return true;
-    }
-
-    setTimeout(reformat, 3000);
-    window.addEventListener('scroll', fillTeachersOnScroll);
-})();
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', function () {
+        reflowDelay();
+    });
+} else {
+    reflowDelay();
+}
