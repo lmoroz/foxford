6458bce964cc59613274b70cc68baf00861127e6
Larisa Moroz <lmoroz@users.noreply.github.com>
2021-09-22 16:48:45 +0800

    Rewrite due to foxford site layout changes

--- foxford.user.js
+++ foxford.user.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?domain=foxford.ru
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
 // @grant        none
+// @global       moment
 // ==/UserScript==
 
 console.log('intercept');
@@ -16,6 +17,44 @@ const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const userTimeZoneOffset = (new Date().getTimezoneOffset())/60;
 const userLocale = Intl.DateTimeFormat().resolvedOptions().locale;
 const arrays = ['course_lessons', 'coach_lessons','events'];
+const timeLineSelectors = ['div[class^="styled__Line-"]', 'div[data-qa="table_Styled.Line"]'];
+const timeZoneSelectors = ['div[class^="moscow-time__Root-"] div[class^="Text_root"]', 'div[data-qa="moscow-time_Root"] div[class*="Text_root"]'];
+const styledBodySelectors = ['div[class^="styled__BodyContainer"]', 'div[data-qa="table_Styled.Body"]'];
+const hourCellSelectors = [`${styledBodySelectors[0]} div[class^="styled__Item"]`, `${styledBodySelectors[1]} div[data-qa="table_Styled.Item"]`];
+const timeWrapperSelectors = ['div[class^="styled__TimeWrapper"]', 'div[data-qa="table_Styled.TimeWrapper"]'];
+const calendarTableBodySelectors = ['div[data-qa="calendar-layout_Styled.Root"]','div[class^="base__ContentContainer"]'];
+const calendarScrollBodySelectors = ['div[data-qa="table_Scrollable"]>div', `${calendarTableBodySelectors[1]} div[style*="overflow: scroll"]:nth-child(2)`];
+
+const ElementsWithScrolls = (function() {
+    const getComputedStyle = (document.body && document.body.currentStyle)
+        ? elem => elem.currentStyle
+        : elem => document.defaultView.getComputedStyle(elem, null);
+
+    function getActualCss(elem, style) {
+        return getComputedStyle(elem)[style];
+    }
+
+    function autoOrScroll(text) {
+        return text == 'scroll' || text == 'auto';
+    }
+
+    function isXScrollable(elem) {
+        return elem.offsetWidth < elem.scrollWidth &&
+            autoOrScroll(getActualCss(elem, 'overflow-x'));
+    }
+
+    function isYScrollable(elem) {
+        return elem.offsetHeight < elem.scrollHeight &&
+            autoOrScroll(getActualCss(elem, 'overflow-y'));
+    }
+
+    function hasScrollerAndScrolled(elem) {
+        return (isYScrollable(elem) || isXScrollable(elem)) && parseInt(elem.scrollTop) !== 0;
+    }
+    return function ElemenetsWithScrolls() {
+        return [].filter.call(document.querySelectorAll('*'), hasScrollerAndScrolled);
+    };
+})();
 
 if (!XMLHttpRequest.prototype.getResponseText) {
     XMLHttpRequest.prototype.getResponseText = Object.getOwnPropertyDescriptor(XMLHttpRequest.prototype, 'responseText').get;
@@ -74,7 +113,7 @@ function reflow(calendarCellElements, calendarTimeElements) {
         }
         const timeCell = calendarTimeElements[timeCellCounter];
         if (!rowFilled) {
-            //console.log(`remove cells fo row №${timeCellCounter+1} : `, cellsToRemove);
+            //console.log(`remove cells for row №${timeCellCounter+1} : `, cellsToRemove);
             //console.log(`remove time row: ${timeCell.innerText}`);
             cellsToRemove.forEach(cell => {cell.parentNode.removeChild(cell);});
             timeCell.parentNode.removeChild(timeCell);
@@ -86,23 +125,33 @@ function reflow(calendarCellElements, calendarTimeElements) {
     }
 }
 function setHoursIds () {
-    document.querySelectorAll(`div[data-qa="table_Styled.Body"] div[data-qa="table_Styled.Item"]`).forEach ((el,id) => {
-        el.dataset.weekHourId = id;
-    });
+    let hourCells = document.querySelectorAll(hourCellSelectors[0]);
+    if (!hourCells.length) hourCells = document.querySelectorAll(hourCellSelectors[1]);
+    if (hourCells.length) {
+        hourCells.forEach ((el,id) => {
+            el.dataset.weekHourId = id;
+        });}
 }
 function repostitionTimeLine() {
-    const calendarTimeLine = document.querySelector('div[data-qa="table_Styled.Line"]');
     const dt = new Date();
+    let calendarTimeLine = document.querySelector(timeLineSelectors[0]);
+    if (!calendarTimeLine) calendarTimeLine = document.querySelector(timeLineSelectors[1]);
     if (calendarTimeLine && calendarTimeLine.parentNode) {
         const currentCellId = (dt.getHours()*7-1)+dt.getDay();
-        const currentCell = document.querySelector(`div[data-qa="table_Styled.Body"] div[data-week-hour-id="${currentCellId}"]`);
+
+        let currentCell = document.querySelector(`${styledBodySelectors[0]} div[data-week-hour-id="${currentCellId}"]`);
+        if (!currentCell) currentCell = document.querySelector(`${styledBodySelectors[1]} div[data-week-hour-id="${currentCellId}"]`);
+
         if (currentCell && !currentCell.isSameNode(calendarTimeLine.parentNode)) {
             console.log('repostitionTimeLine… move to', currentCell);
             calendarTimeLine.parentNode.removeChild(calendarTimeLine);
+
             currentCell.style.backgroundColor = 'lyghtyellow';
-            console.log(currentCell);
+            console.log({currentCell});
             currentCell.appendChild(calendarTimeLine);
-            document.querySelector('div[data-qa="table_Scrollable"]>div').scrollTop = 0;
+
+            let calendarScrolledElements = ElementsWithScrolls();
+            if (calendarScrolledElements.length) calendarScrolledElements.forEach(el => { el.scrollTop = 0; });
         }
     }
     if (calendarTimeLine) {
@@ -116,29 +165,50 @@ function repostitionTimeLine() {
 
 function reflowDelay() {
     const observer = new MutationObserver(function (mutations, me) {
-        const calendarCellElements = document.querySelectorAll('div[data-qa="table_Styled.Body"] div[data-qa="table_Styled.Item"]');
-        const calendarTimeElements = document.querySelectorAll('div[data-qa="table_Styled.TimeWrapper"]>div');
-        const calendarCalendarCard = document.querySelectorAll('div[data-qa="calendar-card_Root"]');
-        const calendarTableBody = document.querySelector('div[data-qa="calendar-layout_Styled.Root"]');
-        const calendarTimeLine = document.querySelector('div[data-qa="table_Styled.Line"]');
-        const calendarTimeZoneText = document.querySelector('div[data-qa="moscow-time_Root"] div[class*="Text_root"]');
-        if (calendarTimeZoneText) calendarTimeZoneText.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        if (calendarTimeLine) { setHoursIds(); repostitionTimeLine(); }
-        if (calendarTableBody && calendarCellElements.length && calendarTimeElements.length === 24 && calendarCalendarCard.length) {
+        let calendarCellElements = document.querySelectorAll(hourCellSelectors[0]);
+        if (!calendarCellElements.length) calendarCellElements = document.querySelectorAll(hourCellSelectors[1]);
+
+        let calendarTimeElements = document.querySelectorAll(`${timeWrapperSelectors[0]}>div`);
+        if (!calendarTimeElements.length) calendarTimeElements = document.querySelectorAll(`${timeWrapperSelectors[1]}>div`);
+
+
+        let calendarTimeLine = document.querySelector(timeLineSelectors[0]);
+        if (!calendarTimeLine) calendarTimeLine = document.querySelector(timeLineSelectors[1]);
+
+        let calendarTimeZoneText = document.querySelector(timeZoneSelectors[0]);
+        if (!calendarTimeZoneText) calendarTimeZoneText = document.querySelector(timeZoneSelectors[1]);
+
+        let calendarTableBody = document.querySelector(calendarTableBodySelectors[0]);
+        if (!calendarTableBody ) calendarTableBody = document.querySelector(calendarTableBodySelectors[1]);
+
+
+        if (calendarTableBody && calendarCellElements.length && calendarTimeElements.length === 24) {
             console.log(`calendarTimeElements = `, calendarTimeElements.length);
             console.log(`calendarCellElements = `, calendarCellElements.length);
-            console.log(`calendarCalendarCard = `, calendarCalendarCard.length);
+
+            if (calendarTimeLine) {
+                setHoursIds();
+                repostitionTimeLine();
+            }
+            if (calendarTimeZoneText) calendarTimeZoneText.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
             reflow(Array.from(calendarCellElements), Array.from(calendarTimeElements));
             me.disconnect(); // stop observing
 
 
 
             const calendarTableBodyOobserver = new MutationObserver(function () {
-                const calendarCellElements = document.querySelectorAll('div[data-qa="table_Styled.Body"] div[data-qa="table_Styled.Item"]');
-                const calendarTimeElements = document.querySelectorAll('div[data-qa="table_Styled.TimeWrapper"]>div');
-                const calendarCalendarCard = document.querySelectorAll('div[data-qa="calendar-card_Root"]');
-                const calendarTimeLine = document.querySelector('div[data-qa="table_Styled.Line"]');
-                if (calendarCellElements.length && calendarTimeElements.length === 24 && calendarCalendarCard.length) {
+
+                let calendarCellElements = document.querySelectorAll(hourCellSelectors[0]);
+                if (!calendarCellElements.length) calendarCellElements = document.querySelectorAll(hourCellSelectors[1]);
+
+                let calendarTimeElements = document.querySelectorAll(`${timeWrapperSelectors[0]}>div`);
+                if (!calendarTimeElements.length) calendarTimeElements = document.querySelectorAll(`${timeWrapperSelectors[1]}>div`);
+
+                let calendarTimeLine = document.querySelector(timeLineSelectors[0]);
+                if (!calendarTimeLine) calendarTimeLine = document.querySelector(timeLineSelectors[1]);
+
+                if (calendarCellElements.length && calendarTimeElements.length === 24) {
                     if (calendarTimeLine) { setHoursIds(); repostitionTimeLine(); }
                     reflow(Array.from(calendarCellElements), Array.from(calendarTimeElements));
                 }
